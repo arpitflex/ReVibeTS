297756077a3f5a3da5100c2f1593aaf7838fef12
Arpit <agrawalarpit007@gmail.com>
2023-03-12 16:54:28 -0400

    Bump discord-player-plus 1.5.0

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,12 @@ import {
     Command,
     createClearCommand,
     createHelpCommand,
+    createInsertCommand,
+    createJumpCommand,
     createPauseCommand,
     createPlayCommand,
     createQueueCommand,
+    createRemoveCommand,
     createRepeatCommand,
     createResumeCommand,
     createSeekCommand,
@@ -16,7 +19,6 @@ import {
     en,
     handleSlashCommand,
     PlayerManager,
-    Track,
     trackToMarkdown,
     Translations,
     urlToMarkdown,
@@ -68,188 +70,6 @@ myPlayCommand.run = async function run(interaction) {
     return true;
 };
 
-const myRemoveCommand: Command = {
-    name: "remove",
-    description: "Removes a song from the queue.",
-    options: [
-        {
-            name: "track_number",
-            description: "Track number of song to remove.",
-            type: ApplicationCommandOptionType.Integer,
-            required: true,
-        },
-    ],
-    run: async (interaction) => {
-        const player = playerManager.find(interaction.guildId);
-
-        if (!player) {
-            await interaction.reply({
-                content: ":robot: I am currently not in the channel.",
-                ephemeral: true,
-            });
-            return false;
-        }
-
-        const queue: Track[] = player.getQueue();
-        const trackNumber: number = interaction.options.getInteger(
-            "track_number",
-            true
-        );
-        const removed: Track | undefined = player.remove(trackNumber - 1);
-
-        if (!removed) {
-            await interaction.reply({
-                content: `Track **${trackNumber}** is not in queue of currently **${queue.length}** tracks`,
-                ephemeral: true,
-            });
-            return false;
-        }
-
-        await interaction.reply({
-            content: `:wastebasket: | Removed **${removed.title}** from queue`,
-        });
-        return true;
-    },
-};
-
-const myJumpCommand: Command = {
-    name: "jump",
-    description: "Jumps to a track in the queue.",
-    options: [
-        {
-            name: "track_number",
-            description: "Track number of song to jump to.",
-            type: ApplicationCommandOptionType.Integer,
-            required: true,
-        },
-    ],
-    run: async (interaction) => {
-        const player = playerManager.find(interaction.guildId);
-
-        if (!player) {
-            await interaction.reply({
-                content: ":robot: I am currently not in the channel.",
-                ephemeral: true,
-            });
-            return false;
-        }
-
-        const channel = player.getVoiceChannel();
-
-        if (!channel) {
-            await interaction.reply({
-                content: ":robot: I am currently not in the voice channel.",
-                ephemeral: true,
-            });
-            return false;
-        }
-
-        const queue: Track[] = player.getQueue();
-        const trackNumber: number = interaction.options.getInteger(
-            "track_number",
-            true
-        );
-
-        if (trackNumber <= 0 || trackNumber > queue.length) {
-            await interaction.reply({
-                content: `Track **${trackNumber}** is not in queue of currently **${queue.length}** tracks`,
-            });
-            return false;
-        }
-
-        player.clear();
-        await player.play({
-            tracks: queue.slice(trackNumber - 1),
-            channel: channel,
-        });
-
-        await interaction.reply({
-            content: `:arrow_right_hook: | Jumped to track **${trackNumber}** in queue`,
-        });
-        return true;
-    },
-};
-
-const myInsertCommand: Command = {
-    name: "insert",
-    description: "Add track to a specific place in the queue",
-    options: [
-        {
-            name: "query",
-            description: playerManager.translations.play.optionDescription,
-            type: ApplicationCommandOptionType.String,
-            required: true
-        },
-        {
-            name: "location_number",
-            description: "Location number to insert the song into.",
-            type: ApplicationCommandOptionType.Integer,
-            required: true
-        }
-    ],
-    run: async (interaction) => {
-        const player = playerManager.find(interaction.guildId);
-
-        if (!player) {
-            await interaction.reply({
-                content: ":robot: I am currently not in the channel.",
-                ephemeral: true,
-            });
-            return false;
-        }
-
-        const channel = player.getVoiceChannel();
-
-        if (!channel) {
-            await interaction.reply({
-                content: ":robot: I am currently not in the voice channel.",
-                ephemeral: true,
-            });
-            return false;
-        }
-
-        const query = interaction.options.getString("query", true);
-        const searchResult = await player.search(query);
-        const firstResult = searchResult[0];
-
-        if (!firstResult || !firstResult.tracks.length) {
-            await interaction.reply({
-                content: playerManager.translations.play.noTracksFound.replace(
-                    "{query}",
-                    query
-                ),
-            });
-            return false;
-        }
-
-        const track = firstResult.tracks[0];
-        const index = interaction.options.getInteger("location_number", true) - 1;
-        player.insert(track, index);
-
-        let readableIndex = index + 1;
-        if (index < 0) {
-            readableIndex = 1;
-        }
-        const queueLength = player.getQueue().length;
-        if (index > queueLength) {
-            readableIndex = queueLength;
-        }
-
-        await interaction.reply({
-            content: playerManager.translations.add.successTrack
-                .replace(
-                    ".",
-                    ` at position ${readableIndex}.`
-                )
-                .replace(
-                    "{track}",
-                    trackToMarkdown(track, true)
-                )
-        });
-        return true;
-    }
-}
-
 // All slash commands for the bot
 const slashCommands: Command[] = [
     createClearCommand(playerManager),
@@ -262,10 +82,10 @@ const slashCommands: Command[] = [
     createStopCommand(playerManager),
     createRepeatCommand(playerManager),
     createSeekCommand(playerManager),
-    myPlayCommand,
-    myRemoveCommand,
-    myJumpCommand,
-    myInsertCommand
+    createInsertCommand(playerManager),
+    createJumpCommand(playerManager),
+    createRemoveCommand(playerManager),
+    myPlayCommand
 ];
 
 slashCommands.push(
