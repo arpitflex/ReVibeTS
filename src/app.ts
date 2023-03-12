59dfba1b5897df9d4b8ca05ed1ca9b6cfd85eddf
Arpit <agrawalarpit007@gmail.com>
2023-03-12 17:00:22 -0400

    Run linter and format

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,38 @@
-import {ActivityType, ApplicationCommandOptionType, Client, GatewayIntentBits,} from "discord.js";
+import { ActivityType, Client, GatewayIntentBits } from "discord.js";
 import {
-    Command,
-    createClearCommand,
-    createHelpCommand,
-    createInsertCommand,
-    createJumpCommand,
-    createPauseCommand,
-    createPlayCommand,
-    createQueueCommand,
-    createRemoveCommand,
-    createRepeatCommand,
-    createResumeCommand,
-    createSeekCommand,
-    createShuffleCommand,
-    createSkipCommand,
-    createSongCommand,
-    createStopCommand,
-    en,
-    handleSlashCommand,
-    PlayerManager,
-    trackToMarkdown,
-    Translations,
-    urlToMarkdown,
+  Command,
+  createClearCommand,
+  createHelpCommand,
+  createInsertCommand,
+  createJumpCommand,
+  createPauseCommand,
+  createPlayCommand,
+  createQueueCommand,
+  createRemoveCommand,
+  createRepeatCommand,
+  createResumeCommand,
+  createSeekCommand,
+  createShuffleCommand,
+  createSkipCommand,
+  createSongCommand,
+  createStopCommand,
+  en,
+  handleSlashCommand,
+  PlayerManager,
+  trackToMarkdown,
+  Translations,
+  urlToMarkdown,
 } from "discord-player-plus";
-import {config} from "./config";
-import {playTracks} from "discord-player-plus/lib/commands";
+import { config } from "./config";
+import { playTracks } from "discord-player-plus/lib/commands";
 
 // Discord client for the Music Bot
 const client: Client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildVoiceStates,
-    ],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
 });
 
 const customTranslations: Translations = en;
@@ -41,76 +41,76 @@ customTranslations.play.description = "Plays a song/playlist.";
 customTranslations.stop.success = ":wave: | Hou doe!";
 
 export const playerManager = new PlayerManager({
-    playerDefault: config.player,
-    translations: customTranslations,
+  playerDefault: config.player,
+  translations: customTranslations,
 });
 
 const myPlayCommand: Command = createPlayCommand(playerManager);
 
 myPlayCommand.run = async function run(interaction) {
-    const searchResult = await playTracks(interaction, playerManager, false);
-    if (!searchResult) return false;
+  const searchResult = await playTracks(interaction, playerManager, false);
+  if (!searchResult) return false;
 
-    if (searchResult.playlist) {
-        await interaction.followUp({
-            content: playerManager.translations.play.successPlaylist.replace(
-                "{playlist}",
-                urlToMarkdown(searchResult.playlist.title, searchResult.playlist.url)
-            ),
-        });
-        return true;
-    }
-
+  if (searchResult.playlist) {
     await interaction.followUp({
-        content: playerManager.translations.play.successTrack.replace(
-            "{track}",
-            trackToMarkdown(searchResult.tracks[0])
-        ),
+      content: playerManager.translations.play.successPlaylist.replace(
+        "{playlist}",
+        urlToMarkdown(searchResult.playlist.title, searchResult.playlist.url)
+      ),
     });
     return true;
+  }
+
+  await interaction.followUp({
+    content: playerManager.translations.play.successTrack.replace(
+      "{track}",
+      trackToMarkdown(searchResult.tracks[0])
+    ),
+  });
+  return true;
 };
 
 // All slash commands for the bot
 const slashCommands: Command[] = [
-    createClearCommand(playerManager),
-    createPauseCommand(playerManager),
-    createQueueCommand(playerManager, {ephemeral: true}),
-    createResumeCommand(playerManager),
-    createShuffleCommand(playerManager),
-    createSkipCommand(playerManager),
-    createSongCommand(playerManager, {ephemeral: true}),
-    createStopCommand(playerManager),
-    createRepeatCommand(playerManager),
-    createSeekCommand(playerManager),
-    createInsertCommand(playerManager),
-    createJumpCommand(playerManager),
-    createRemoveCommand(playerManager),
-    myPlayCommand
+  createClearCommand(playerManager),
+  createPauseCommand(playerManager),
+  createQueueCommand(playerManager, { ephemeral: true }),
+  createResumeCommand(playerManager),
+  createShuffleCommand(playerManager),
+  createSkipCommand(playerManager),
+  createSongCommand(playerManager, { ephemeral: true }),
+  createStopCommand(playerManager),
+  createRepeatCommand(playerManager),
+  createSeekCommand(playerManager),
+  createInsertCommand(playerManager),
+  createJumpCommand(playerManager),
+  createRemoveCommand(playerManager),
+  myPlayCommand,
 ];
 
 slashCommands.push(
-    createHelpCommand(playerManager, {
-        commands: slashCommands,
-        title: "ReVibe",
-        author: {
-            name: "Arpit Agrawal",
-        },
-        footerText: "Thanks for using ReVibe",
-    })
+  createHelpCommand(playerManager, {
+    commands: slashCommands,
+    title: "ReVibe",
+    author: {
+      name: "Arpit Agrawal",
+    },
+    footerText: "Thanks for using ReVibe",
+  })
 );
 
 client
-    .on("ready", async (client) => {
-        console.log(`Bot ready and logged in as ${client.user.tag}`);
-        await client.application.commands.set(slashCommands);
-        client.user.setActivity({name: "/help", type: ActivityType.Listening});
-    })
-    .on("interactionCreate", async (interaction) => {
-        await handleSlashCommand(
-            interaction,
-            slashCommands,
-            playerManager.translations
-        );
-    });
+  .on("ready", async (client) => {
+    console.log(`Bot ready and logged in as ${client.user.tag}`);
+    await client.application.commands.set(slashCommands);
+    client.user.setActivity({ name: "/help", type: ActivityType.Listening });
+  })
+  .on("interactionCreate", async (interaction) => {
+    await handleSlashCommand(
+      interaction,
+      slashCommands,
+      playerManager.translations
+    );
+  });
 
 client.login(config.app.clientToken);
